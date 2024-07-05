000d53c28c89aaf924fcae0eb8a6d5d67b05a07e
hanaru <133945249+1roo@users.noreply.github.com>
2024-07-05 22:04:25 +0900

    정규표현식 validator / transformation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -2,6 +2,10 @@
 
 import { z } from "zod";
 
+const passwordRegex = new RegExp(
+  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$/
+);
+
 const checkUserName = (username: string) => !username.includes("patato");
 
 const checkPasswords = ({
@@ -21,9 +25,18 @@ const formSchema = z
       })
       .min(3, "Way too short!!")
       .max(10, "That is too looong!!")
+      .toLowerCase()
+      .trim()
+      .transform((username) => `👀 ${username}`)
       .refine(checkUserName, "No potatoes allowed"),
-    email: z.string().email(),
-    password: z.string().min(10),
+    email: z.string().email().toLowerCase(),
+    password: z
+      .string()
+      .min(10)
+      .regex(
+        passwordRegex,
+        "A password must have lowercase, UPPERCASE, a number and special characters."
+      ),
     confirm_password: z.string().min(10),
   })
   .refine(checkPasswords, {
@@ -51,5 +64,7 @@ export async function createAccount(prevState: any, formData: FormData) {
     console.log(result.error.flatten());
 
     return result.error.flatten();
+  } else {
+    console.log(result.data);
   }
 }
