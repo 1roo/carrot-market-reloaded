5b194d7ba90cdb90c3bfad7863c684ad5fff3553
hanaru <133945249+1roo@users.noreply.github.com>
2024-07-10 12:36:20 +0900

    matcher

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,9 @@
-import { cookies } from "next/headers";
 import { NextRequest } from "next/server";
-import getSession from "./lib/session";
 
 export async function middleware(request: NextRequest) {
-  console.log(cookies());
-  const session = await getSession();
-  console.log(session);
-
-  if (request.nextUrl.pathname === "/profile") {
-    return Response.redirect(new URL("/", request.url));
-  }
+  console.log("hello");
 }
+
+export const config = {
+  matcher: ["/", "/profile", "/create-account", "/user/:path*"],
+};
