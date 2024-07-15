3983bd47323f4713a9013c1f4d74338606829624
hanaru <133945249+1roo@users.noreply.github.com>
2024-07-15 18:26:50 +0900

    Cloudflare에 업로드 url 요청

--- app/products/add/actions.ts
+++ app/products/add/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { z } from "zod";
-import fs from "fs/promises";
+// import fs from "fs/promises";
 import db from "@/lib/db";
 import getSession from "@/lib/session";
 import { redirect } from "next/navigation";
@@ -28,12 +28,12 @@ export async function uploadProduct(_: any, formData: FormData) {
     price: formData.get("price"),
     description: formData.get("description"),
   };
-  if (data.photo instanceof File) {
-    //유저가 업로드한 파일을
-    const photoData = await data.photo.arrayBuffer(); //바이트 형식으로 읽어들여서
-    await fs.appendFile(`/public/${data.photo.name}`, Buffer.from(photoData)); //원하는 경로에 생성한 파일 안에 그대로 넣어줌.
-    data.photo = `/${data.photo.name}`;
-  }
+  // if (data.photo instanceof File) {
+  //   //유저가 업로드한 파일을
+  //   const photoData = await data.photo.arrayBuffer(); //바이트 형식으로 읽어들여서
+  //   await fs.appendFile(`/public/${data.photo.name}`, Buffer.from(photoData)); //원하는 경로에 생성한 파일 안에 그대로 넣어줌.
+  //   data.photo = `/${data.photo.name}`;
+  // }
   const result = productSchema.safeParse(data);
   if (!result.success) {
     return result.error.flatten();
@@ -59,4 +59,18 @@ export async function uploadProduct(_: any, formData: FormData) {
       redirect(`/products/${product.id}`);
     }
   }
+}
+
+export async function getUploadUrl() {
+  const response = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/images/v2/direct_upload`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.CLOUDFLARE_API_KEY}`,
+      },
+    }
+  );
+  const data = await response.json();
+  return data;
 }
