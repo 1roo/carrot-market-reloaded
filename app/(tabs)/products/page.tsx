3e6f30507f89f14d5e26ff53a49b5ff98a726891
hanaru <133945249+1roo@users.noreply.github.com>
2024-07-15 10:58:14 +0900

    페이지네이션

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,7 +1,9 @@
 import ListProduct from "@/components/list-product";
+import ProductList from "@/components/product-list";
 import db from "@/lib/db";
+import { Prisma } from "@prisma/client";
 
-async function getProducts() {
+async function getInitialProducts() {
   const products = await db.product.findMany({
     select: {
       title: true,
@@ -10,17 +12,25 @@ async function getProducts() {
       photo: true,
       id: true,
     },
+    take: 1,
+    orderBy: {
+      created_at: "desc",
+    },
   });
   return products;
 }
 
+//타입 알고싶을때!!
+export type InitialProducts = Prisma.PromiseReturnType<
+  typeof getInitialProducts
+>;
+//그 다음 interface에 나열대신 InitialProducts만 쓰면 됨.
+
 export default async function Products() {
-  const products = await getProducts();
+  const initialProducts = await getInitialProducts();
   return (
-    <div className="p-5 flex flex-col gap-5">
-      {products.map((product) => (
-        <ListProduct key={product.id} {...product} />
-      ))}
+    <div>
+      <ProductList initialProducts={initialProducts} />
     </div>
   );
 }
